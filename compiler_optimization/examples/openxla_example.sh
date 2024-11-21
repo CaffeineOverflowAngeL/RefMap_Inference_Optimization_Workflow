e81c399e195a587ae18c30d7134ce42d5f2cdab1
Angelos Maroudis <angelosmar33@gmail.com>
2024-11-21 13:48:31 +0200

    Example Update

--- compiler_optimization/examples/openxla_example.sh
+++ compiler_optimization/examples/openxla_example.sh
@@ -5,8 +5,8 @@ echo "Inference Optimization Example for the midterm demonstration."
 
 # Baseline # 
 
-python3 xla_inference.py --use_native_tensorflow --restore-format hdf5
+python3 compiler_optimization/xla_inference.py --use_native_tensorflow --restore-format hdf5
 
 # XLA Optimised # 
 
-python3 xla_inference.py --use_xla_model --restore-format hdf5
+python3 compiler_optimization/xla_inference.py --use_xla_model --restore-format hdf5
